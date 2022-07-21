8aab6ba19f79afc121af8ec5a898e28db92d9800
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-07-21 11:28:04 +0200

    Updated jetpack compose dependency Removed label from manifest

--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/theme/Theme.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/theme/Theme.kt
@@ -28,10 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun IdeeFixeCreator2Theme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
+fun IdeeFixeCreator2Theme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
