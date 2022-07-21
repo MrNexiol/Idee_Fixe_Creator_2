0bb8df9e35f8ad94c59efe65760f8c691eca84ff
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-07-21 14:12:23 +0200

    Added character sheet model

---
+++ domain/src/main/java/tomasz/kopycinski/domain/model/CharacterSheet.kt
@@ -0,0 +1,17 @@
+package tomasz.kopycinski.domain.model
+
+import java.util.Date
+
+data class CharacterSheet(
+    val name: String,
+    val surname: String,
+    val age: Int,
+    val gender: String,
+    val dateOfBirth: Date,
+    var destinyPoints: Int,
+    var luckPoints: Int,
+    var accountMoney: Double,
+    var jewelryMoney: Double,
+    var barterMoney: Double,
+    var notes: String
+)
