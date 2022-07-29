a4c43e430cae7d5d3bd4ebda20d9616772e8b929
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-07-29 09:59:13 +0200

    Added domain dependency to data module Added converter CharacterSheet domain-entity

---
+++ data/src/main/java/tomasz/kopycinski/data/converter/CharacterSheetConverter.kt
@@ -0,0 +1,16 @@
+package tomasz.kopycinski.data.converter
+
+import tomasz.kopycinski.data.entity.CharacterSheet as EntityCharacterSheet
+import tomasz.kopycinski.domain.model.CharacterSheet as DomainCharacterSheet
+
+fun EntityCharacterSheet.toDomain(): DomainCharacterSheet {
+    return DomainCharacterSheet(
+        id, name, surname, age, gender, dateOfBirth, destinyPoints, luckPoints, accountMoney, jewelryMoney, barterMoney, notes
+    )
+}
+
+fun DomainCharacterSheet.toEntity(): EntityCharacterSheet {
+    return EntityCharacterSheet(
+        name, surname, age, gender, dateOfBirth, destinyPoints, luckPoints, accountMoney, jewelryMoney, barterMoney, notes, id
+    )
+}
