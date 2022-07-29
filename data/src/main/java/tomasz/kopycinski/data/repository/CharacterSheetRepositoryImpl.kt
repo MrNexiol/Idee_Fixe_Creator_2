f118d2fadb57c2c72ac7ef749b36ddf674f388a5
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-07-29 11:04:51 +0200

    Added basic implementation of CharacterSheetRepository Refactored CharacterSheetRepository delete method

---
+++ data/src/main/java/tomasz/kopycinski/data/repository/CharacterSheetRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package tomasz.kopycinski.data.repository
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import tomasz.kopycinski.data.converter.toDomain
+import tomasz.kopycinski.data.converter.toEntity
+import tomasz.kopycinski.data.dao.CharacterSheetDao
+import tomasz.kopycinski.domain.model.CharacterSheet
+import tomasz.kopycinski.domain.repository.CharacterSheetRepository
+
+class CharacterSheetRepositoryImpl(
+    private val characterSheetDao: CharacterSheetDao
+) : CharacterSheetRepository {
+    override suspend fun getCharacterSheet(id: Int): CharacterSheet =
+        characterSheetDao.getOneById(id).toDomain()
+
+    override suspend fun deleteCharacterSheet(characterSheet: CharacterSheet) =
+        characterSheetDao.delete(characterSheet.toEntity())
+
+    override suspend fun saveCharacterSheet(characterSheet: CharacterSheet) =
+        characterSheetDao.insert(characterSheet.toEntity())
+
+    override suspend fun getListOfCharacterSheets(): Flow<List<CharacterSheet>> =
+        characterSheetDao.getAll().map { list -> list.map { it.toDomain() } }
+}
--- domain/src/main/java/tomasz/kopycinski/domain/repository/CharacterSheetRepository.kt
+++ domain/src/main/java/tomasz/kopycinski/domain/repository/CharacterSheetRepository.kt
@@ -6,7 +6,7 @@ import tomasz.kopycinski.domain.model.CharacterSheet
 interface CharacterSheetRepository {
     suspend fun getCharacterSheet(id: Int): CharacterSheet
 
-    suspend fun deleteCharacterSheet(id: Int)
+    suspend fun deleteCharacterSheet(characterSheet: CharacterSheet)
 
     suspend fun saveCharacterSheet(characterSheet: CharacterSheet)
 
