f14733cc5f62b8468234c091803ea8f9700ef562
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-08-01 12:10:46 +0200

    Added empty method for CharacterSheetCreator

---
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetcreator/CharacterSheetCreator.kt
@@ -0,0 +1,9 @@
+package tomasz.kopycinski.ideefixecreator2.ui.screens.charactersheetcreator
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CharacterSheetCreator() {
+    Text(text = "Creator")
+}
