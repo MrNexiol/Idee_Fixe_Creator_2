50cf725102863305283040468a002cbb5574326d
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-08-01 11:26:32 +0200

    Added getting list of character sheets in CharacterSheetListViewModel

--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetlist/CharacterSheetListViewModel.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetlist/CharacterSheetListViewModel.kt
@@ -1,11 +1,30 @@
 package tomasz.kopycinski.ideefixecreator2.ui.screens.charactersheetlist
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import tomasz.kopycinski.domain.model.CharacterSheet
 import tomasz.kopycinski.domain.usecase.GetListOfCharacterSheetsUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 class CharacterSheetListViewModel @Inject constructor(
     private val getListOfCharacterSheetsUseCase: GetListOfCharacterSheetsUseCase
-) : ViewModel()
+) : ViewModel() {
+
+    private var _characterSheets = MutableStateFlow<List<CharacterSheet>>(listOf())
+
+    val characterSheets: StateFlow<List<CharacterSheet>> = _characterSheets.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            getListOfCharacterSheetsUseCase().collect {
+                _characterSheets.value = it
+            }
+        }
+    }
+}
