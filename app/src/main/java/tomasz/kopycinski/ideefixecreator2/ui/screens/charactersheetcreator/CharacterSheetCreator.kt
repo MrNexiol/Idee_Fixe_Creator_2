58acdea6cfcc2d1aeb07dd275bad468dace4f08f
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-08-01 12:19:40 +0200

    Injected viewModel in CharacterSheetCreator

--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetcreator/CharacterSheetCreator.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetcreator/CharacterSheetCreator.kt
@@ -2,8 +2,11 @@ package tomasz.kopycinski.ideefixecreator2.ui.screens.charactersheetcreator
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun CharacterSheetCreator() {
+fun CharacterSheetCreator(
+    viewModel: CharacterSheetCreatorViewModel = hiltViewModel()
+) {
     Text(text = "Creator")
 }
--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetcreator/CharacterSheetCreatorViewModel.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetcreator/CharacterSheetCreatorViewModel.kt
@@ -2,7 +2,8 @@ package tomasz.kopycinski.ideefixecreator2.ui.screens.charactersheetcreator
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class CharacterSheetCreatorViewModel : ViewModel() {
+class CharacterSheetCreatorViewModel @Inject constructor() : ViewModel() {
 }
