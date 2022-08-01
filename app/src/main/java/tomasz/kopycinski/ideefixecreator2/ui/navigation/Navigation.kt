1c092b5524f67ec4d4e365487c78ee0213d355c0
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-08-01 12:12:54 +0200

    Added CharacterSheetCreator to navigation

--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/navigation/Navigation.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/navigation/Navigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import tomasz.kopycinski.ideefixecreator2.ui.screens.charactersheetcreator.CharacterSheetCreator
 import tomasz.kopycinski.ideefixecreator2.ui.screens.charactersheetlist.CharacterSheetList
 
 @Composable
@@ -11,6 +12,9 @@ fun Navigation(navHostController: NavHostController) {
     NavHost(navController = navHostController, startDestination = Screen.CharacterSheetList.route) {
         composable(Screen.CharacterSheetList.route) {
             CharacterSheetList()
+        }
+        composable(Screen.CharacterSheetCreator.route) {
+            CharacterSheetCreator()
         }
     }
 }
