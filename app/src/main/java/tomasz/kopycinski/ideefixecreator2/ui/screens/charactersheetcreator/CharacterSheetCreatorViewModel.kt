46ddbb2fbd98546ac75307c4b6273ad8ac5ff616
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-08-01 12:22:02 +0200

    Added method to save character sheet to CharacterSheetCreatorViewModel

--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetcreator/CharacterSheetCreatorViewModel.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetcreator/CharacterSheetCreatorViewModel.kt
@@ -1,12 +1,24 @@
 package tomasz.kopycinski.ideefixecreator2.ui.screens.charactersheetcreator
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import tomasz.kopycinski.domain.model.CharacterSheet
 import tomasz.kopycinski.domain.usecase.SaveCharacterSheetUseCase
+import java.util.Date
 import javax.inject.Inject
 
 @HiltViewModel
 class CharacterSheetCreatorViewModel @Inject constructor(
-    saveCharacterSheetUseCase: SaveCharacterSheetUseCase
+    private val saveCharacterSheetUseCase: SaveCharacterSheetUseCase
 ) : ViewModel() {
+
+    private val characterSheet =
+        CharacterSheet(0, "Preview", "Character", 21, "Male", Date(), 150, 0, 10000.0, 0.0, 0.0, "")
+
+
+    fun saveCharacterSheet() = viewModelScope.launch {
+        saveCharacterSheetUseCase(characterSheet)
+    }
 }
