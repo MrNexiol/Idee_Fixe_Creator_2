55ac43635facd1791d885b044454dc2d192c124a
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-08-01 12:11:17 +0200

    Added CharacterSheetCreator to screen class

--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/navigation/Screen.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/navigation/Screen.kt
@@ -2,4 +2,5 @@ package tomasz.kopycinski.ideefixecreator2.ui.navigation
 
 sealed class Screen(val route: String) {
     object CharacterSheetList : Screen("charactersheet_list")
+    object CharacterSheetCreator : Screen("charactersheet_creator")
 }
