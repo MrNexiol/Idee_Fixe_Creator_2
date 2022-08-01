918423b058b623501159d259b8bd86d2eb171f1a
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-08-01 12:18:39 +0200

    Added CharacterSheetCreatorViewModel

---
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetcreator/CharacterSheetCreatorViewModel.kt
@@ -0,0 +1,8 @@
+package tomasz.kopycinski.ideefixecreator2.ui.screens.charactersheetcreator
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class CharacterSheetCreatorViewModel : ViewModel() {
+}
