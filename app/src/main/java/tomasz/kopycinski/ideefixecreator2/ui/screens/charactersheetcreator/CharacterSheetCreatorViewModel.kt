0b491bd70b0e31d4c092f86db8535acfafaf8f54
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-08-01 12:20:32 +0200

    Injected SaveCharacterSheetUseCase into CharacterSheetCreatorViewModel

--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetcreator/CharacterSheetCreatorViewModel.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetcreator/CharacterSheetCreatorViewModel.kt
@@ -2,8 +2,11 @@ package tomasz.kopycinski.ideefixecreator2.ui.screens.charactersheetcreator
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import tomasz.kopycinski.domain.usecase.SaveCharacterSheetUseCase
 import javax.inject.Inject
 
 @HiltViewModel
-class CharacterSheetCreatorViewModel @Inject constructor() : ViewModel() {
+class CharacterSheetCreatorViewModel @Inject constructor(
+    saveCharacterSheetUseCase: SaveCharacterSheetUseCase
+) : ViewModel() {
 }
--- domain/src/main/java/tomasz/kopycinski/domain/usecase/SaveCharacterSheetUseCase.kt
+++ domain/src/main/java/tomasz/kopycinski/domain/usecase/SaveCharacterSheetUseCase.kt
@@ -2,8 +2,9 @@ package tomasz.kopycinski.domain.usecase
 
 import tomasz.kopycinski.domain.model.CharacterSheet
 import tomasz.kopycinski.domain.repository.CharacterSheetRepository
+import javax.inject.Inject
 
-class SaveCharacterSheetUseCase(
+class SaveCharacterSheetUseCase @Inject constructor(
     private val repository: CharacterSheetRepository
 ) {
     suspend operator fun invoke(characterSheet: CharacterSheet) =
