081d0458370d7e81b6eb6e200edb8fe61f930756
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-08-03 10:30:09 +0200

    Changed app primary color

--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/theme/Color.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/theme/Color.kt
@@ -2,7 +2,10 @@ package tomasz.kopycinski.ideefixecreator2.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+const val blue700 = 0xFF1976d2
+
+val primary = Color(blue700)
+
 val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
