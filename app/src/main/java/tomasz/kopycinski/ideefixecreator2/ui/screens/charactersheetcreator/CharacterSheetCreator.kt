c5062a7c97f4adec17454fc085e7d31d7a754d99
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-08-03 10:38:29 +0200

    Changed hardcoded strings to stringResources

--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetcreator/CharacterSheetCreator.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetcreator/CharacterSheetCreator.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.hilt.navigation.compose.hiltViewModel
+import tomasz.kopycinski.ideefixecreator2.R
 import tomasz.kopycinski.ideefixecreator2.ui.components.TextInput
 
 @Composable
@@ -19,34 +21,34 @@ fun CharacterSheetCreator(
         TextInput(
             value = uiState.name,
             onValueChange = { viewModel.changeName(it) },
-            label = "Name"
+            label = stringResource(id = R.string.name)
         )
         TextInput(
             value = uiState.surname,
             onValueChange = { viewModel.changeSurName(it) },
-            label = "Surname"
+            label = stringResource(id = R.string.surname)
         )
         TextInput(
             value = uiState.age.toString(),
             onValueChange = { viewModel.changeAge(it) },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            label = "Age"
+            label = stringResource(id = R.string.age)
         )
         TextInput(
             value = uiState.gender,
             onValueChange = { viewModel.changeGender(it) },
-            label = "Gender"
+            label = stringResource(id = R.string.gender)
         )
         TextInput(
             value = uiState.experience.toString(),
             onValueChange = { viewModel.changeExperience(it) },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            label = "Starting experience"
+            label = stringResource(id = R.string.experience)
         )
         TextInput(
             value = uiState.notes,
             onValueChange = { viewModel.changeNotes(it) },
-            label = "Notes"
+            label = stringResource(id = R.string.notes)
         )
         Button(onClick = { viewModel.saveCharacterSheet() }) {
             Text(text = "Save character sheet")
