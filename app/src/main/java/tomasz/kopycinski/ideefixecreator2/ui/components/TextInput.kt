a58d475c843e39825f992493e78c5a89bfbe764b
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-08-03 09:40:55 +0200

    Added custom HalfCutCorner shape Used custom shape on textInput

--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/components/TextInput.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/components/TextInput.kt
@@ -4,20 +4,24 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import tomasz.kopycinski.ideefixecreator2.ui.theme.HalfCutCornerShape
 
 @Composable
 fun TextInput(
     value: String,
     onValueChange: (String) -> Unit,
     label: String = "",
-    keyboardOptions: KeyboardOptions = KeyboardOptions.Default
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    shape: Shape = HalfCutCornerShape()
 ) {
     OutlinedTextField(
         value = value,
-        label = { Text(text = label) },
         onValueChange = onValueChange,
-        keyboardOptions = keyboardOptions
+        label = { Text(text = label) },
+        keyboardOptions = keyboardOptions,
+        shape = shape
     )
 }
 
--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/theme/Shape.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/theme/Shape.kt
@@ -2,6 +2,12 @@ package tomasz.kopycinski.ideefixecreator2.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
@@ -9,3 +15,24 @@ val Shapes = Shapes(
     medium = RoundedCornerShape(4.dp),
     large = RoundedCornerShape(0.dp)
 )
+
+class HalfCutCornerShape(private val cornerRadius: Float = 32f): Shape {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        return Outline.Generic(
+            path = Path().apply {
+                moveTo(0f + cornerRadius, 0f)
+                lineTo(size.width, 0f)
+                lineTo(size.width, size.height - cornerRadius)
+                lineTo(size.width - cornerRadius, size.height)
+                lineTo(0f, size.height)
+                lineTo(0f, 0f + cornerRadius)
+                close()
+            }
+        )
+    }
+
+}
