391e879d0dd42d54294f114a3b8eac53a203aa0d
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-08-13 16:30:13 +0200

    Added border to CharacterSheetListItem

--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/components/CharacterSheetListItem.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/components/CharacterSheetListItem.kt
@@ -1,15 +1,18 @@
 package tomasz.kopycinski.ideefixecreator2.ui.components
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import tomasz.kopycinski.domain.model.CharacterSheet
+import tomasz.kopycinski.ideefixecreator2.ui.theme.HalfCutCornerShape
 import java.util.Date
 
 @Composable
@@ -17,7 +20,11 @@ fun CharacterSheetListItem(
     characterSheet: CharacterSheet
 ) {
     Row(
-        modifier = Modifier.fillMaxWidth().padding(8.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+            .border(2.dp, MaterialTheme.colors.primary, HalfCutCornerShape())
+            .padding(8.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(text = characterSheet.fullName())
