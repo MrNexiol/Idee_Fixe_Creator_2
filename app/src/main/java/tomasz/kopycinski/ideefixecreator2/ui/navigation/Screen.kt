6198e2255b63704bf119e7939e58fd24b74c1a10
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-08-14 12:25:14 +0200

    Added CharacterSheetDetailsRoute to Screen class

--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/navigation/Screen.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/navigation/Screen.kt
@@ -3,4 +3,7 @@ package tomasz.kopycinski.ideefixecreator2.ui.navigation
 sealed class Screen(val route: String) {
     object CharacterSheetList : Screen("charactersheet_list")
     object CharacterSheetCreator : Screen("charactersheet_creator")
+    object CharacterSheetDetails : Screen("charactersheet_details/{id}") {
+        fun createRoute(id: String): String = "$route/$id)"
+    }
 }
