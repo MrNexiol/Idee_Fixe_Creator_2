b975e3fe6524b24e90adf64f11c919604739187b
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-08-14 12:30:23 +0200

    Injected viewModel in CharacterSheetDetailsScreen

--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetdetails/CharacterSheetDetailsScreen.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetdetails/CharacterSheetDetailsScreen.kt
@@ -2,10 +2,12 @@ package tomasz.kopycinski.ideefixecreator2.ui.screens.charactersheetdetails
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun CharacterSheetDetailsScreen(
-    characterSheetId: Int
+    characterSheetId: Int,
+    viewModel: CharacterSheetDetailsViewModel = hiltViewModel()
 ) {
     Text(text = "Id = $characterSheetId")
 }
