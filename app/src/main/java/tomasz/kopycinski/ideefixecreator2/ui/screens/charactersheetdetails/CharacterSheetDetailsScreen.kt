32a4d18b68d2477ef6d80d32cd76bc17bc408f6f
Tomasz Kopyciński <tkopycinski@prograils.com>
2022-08-16 10:39:48 +0200

    Added collecting character sheet info from database

--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetdetails/CharacterSheetDetailsScreen.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetdetails/CharacterSheetDetailsScreen.kt
@@ -2,6 +2,7 @@ package tomasz.kopycinski.ideefixecreator2.ui.screens.charactersheetdetails
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -9,5 +10,15 @@ fun CharacterSheetDetailsScreen(
     characterSheetId: Int,
     viewModel: CharacterSheetDetailsViewModel = hiltViewModel()
 ) {
-    Text(text = "Id = $characterSheetId")
+    val uiState = viewModel.uiState
+
+    LaunchedEffect(Unit) {
+        viewModel.updateUiState(characterSheetId)
+    }
+
+    if (uiState.loading) {
+        Text(text = "Loading")
+    } else {
+        Text(text = "Id = ${uiState.characterSheet!!.surname}")
+    }
 }
--- app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetdetails/CharacterSheetDetailsViewModel.kt
+++ app/src/main/java/tomasz/kopycinski/ideefixecreator2/ui/screens/charactersheetdetails/CharacterSheetDetailsViewModel.kt
@@ -1,12 +1,33 @@
 package tomasz.kopycinski.ideefixecreator2.ui.screens.charactersheetdetails
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import tomasz.kopycinski.domain.model.CharacterSheet
 import tomasz.kopycinski.domain.usecase.GetSingleCharacterSheetUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 class CharacterSheetDetailsViewModel @Inject constructor(
-    getSingleCharacterSheetUseCase: GetSingleCharacterSheetUseCase
+    private val getSingleCharacterSheetUseCase: GetSingleCharacterSheetUseCase,
 ) : ViewModel() {
+
+    var uiState by mutableStateOf(CharacterSheetDetailsUiState())
+        private set
+
+    fun updateUiState(id: Int) = viewModelScope.launch {
+        uiState = uiState.copy(
+            loading = false,
+            characterSheet = getSingleCharacterSheetUseCase(id)
+        )
+    }
 }
+
+data class CharacterSheetDetailsUiState(
+    val loading: Boolean = true,
+    val characterSheet: CharacterSheet? = null
+)
